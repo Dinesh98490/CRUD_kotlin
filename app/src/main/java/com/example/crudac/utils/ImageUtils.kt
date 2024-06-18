f6cc3b6a70618666e2315e2c7d8b92ebb447a2f4
“Dinesh98490” <kunwardinesh028@gmail.com>
2024-06-18 18:21:40 +0545

    data insert in imageutils

--- app/src/main/java/com/example/crudac/utils/ImageUtils.kt
+++ app/src/main/java/com/example/crudac/utils/ImageUtils.kt
@@ -1,5 +1,51 @@
 package com.example.crudac.utils
 
-class ImageUtils {
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+
+    class Imageutils(private val activity: AppCompatActivity) {
+
+        private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
+        fun registerActivity(callback: (Uri?) -> Unit) {
+            activityResultLauncher = activity.registerForActivityResult(
+                ActivityResultContracts.StartActivityForResult(),
+                ActivityResultCallback { result ->
+                    val resultCode = result.resultCode
+                    val imageData = result.data
+                    if (resultCode == AppCompatActivity.RESULT_OK && imageData != null) {
+                        val imageUri = imageData.data
+                        callback(imageUri)
+                    }
+                }
+            )
+        }
+
+        fun launchGallery(context: Context) {
+            val permissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                android.Manifest.permission.READ_MEDIA_IMAGES
+            } else {
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            }
+            if (ContextCompat.checkSelfPermission(context, permissions) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, arrayOf(permissions), 1)
+            } else {
+                val intent = Intent()
+                intent.type = "image/*"
+                intent.action = Intent.ACTION_GET_CONTENT
+                activityResultLauncher.launch(intent)
+            }
+        }
+    }
 
 }
